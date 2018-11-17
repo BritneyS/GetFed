a116d3aac6fe0733ab521b8e2f51c08d5fb603f6
Britney Smith <brit.smith7@gmail.com>
2018-11-16 22:51:58 -0500

    Added HomeViewController.swift

---
+++ GetFed/GetFed/Controllers/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  GetFed
+//
+//  Created by Britney Smith on 11/16/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var foodSearchButton: UIButton!
+}
