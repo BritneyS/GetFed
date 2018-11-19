427bc2728d23d111301645eecd1318620516d938
Britney Smith <brit.smith7@gmail.com>
2018-11-19 14:41:48 -0500

    WIP - Data Manager

---
+++ GetFed/GetFed/Helpers/DataManager.swift
@@ -0,0 +1,22 @@
+//
+//  DataManager.swift
+//  GetFed
+//
+//  Created by Britney Smith on 11/19/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    let appId = EdamamAppID
+    let appKey = EdamamAppKey
+    var url = ""
+    var text = ""
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+
+}
