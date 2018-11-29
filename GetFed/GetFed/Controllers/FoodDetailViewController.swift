bd535318dd0383e081b2c278d983ab413da8f76a
Britney Smith <brit.smith7@gmail.com>
2018-11-29 11:04:14 -0500

    Refactor of chart creation

--- GetFed/GetFed/Controllers/FoodDetailViewController.swift
+++ GetFed/GetFed/Controllers/FoodDetailViewController.swift
@@ -27,15 +27,17 @@ class FoodDetailViewController: UIViewController {
     var carbs: Double?
     var fat: Double?
     var macroNutrientData: [Double?] = []
-
+    //var dataSet: PieChartDataSet?
     
     // MARK - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         populateLabels()
-        populateMacroNutrientChartData()
+        let dataSet = populateMacroNutrientChartData()
+        //guard let dataSet = dataSet else { return }
+        formatChartValues(with: dataSet)
+        styleMacroNutrientChart(with: dataSet)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -100,20 +102,20 @@ class FoodDetailViewController: UIViewController {
 
 // MARK - Pie Chart Methods
 extension FoodDetailViewController {
-    func populateMacroNutrientChartData() {
+    func populateMacroNutrientChartData() -> PieChartDataSet? {
         guard let proteinData = self.protein else {
             print("No protein data")
-            return
+            return nil
         }
         
         guard let carbsData = self.carbs else {
             print("No carbs data")
-            return
+            return nil
         }
         
         guard let fatData = self.fat else {
             print("No fat data")
-            return
+            return nil
         }
         
         let entryOne = PieChartDataEntry(value: proteinData, label: "Protein")
@@ -122,29 +124,58 @@ extension FoodDetailViewController {
         let dataEntries = [entryOne, entryTwo, entryThree]
         
         let dataSet = PieChartDataSet(values: dataEntries, label: "samplechartlabel")
+        //guard let dataSet = dataSet else { return }
         let data = PieChartData(dataSet: dataSet)
         macroNutrientChart.data = data
         
         /// Chart customization
-        let chartLabelColor = UIColor(red:0.38, green:0.07, blue:0.33, alpha:1.0)
-        dataSet.colors = ChartColorTemplates.customTemplateBright()
-        dataSet.valueTextColor = chartLabelColor
-        dataSet.valueFont = UIFont(name:"HelveticaNeue-Bold", size: 18.0)!
-        dataSet.xValuePosition = .outsideSlice
-        dataSet.yValuePosition = .outsideSlice
-        dataSet.valueLineColor = chartLabelColor
-        dataSet.valueLinePart1Length = 0.45
-        dataSet.valueLinePart2Length = 0.1
+//        let chartLabelColor = UIColor(red:0.38, green:0.07, blue:0.33, alpha:1.0)
+//        dataSet.colors = ChartColorTemplates.customTemplateBright()
+//        dataSet.valueTextColor = chartLabelColor
+//        dataSet.valueFont = UIFont(name:"HelveticaNeue-Bold", size: 18.0)!
+//        dataSet.xValuePosition = .outsideSlice
+//        dataSet.yValuePosition = .outsideSlice
+//        dataSet.valueLineColor = chartLabelColor
+//        dataSet.valueLinePart1Length = 0.45
+//        dataSet.valueLinePart2Length = 0.1
         
-        let measurementFormatter = MeasurementFormatter()
-        let customValueFormatter = CustomValueFormatter(measurementFormatter: measurementFormatter)
-        dataSet.valueFormatter = customValueFormatter
+//        let measurementFormatter = MeasurementFormatter()
+//        let customValueFormatter = CustomValueFormatter(measurementFormatter: measurementFormatter)
+//        dataSet.valueFormatter = customValueFormatter
         
-        macroNutrientChart.animate(yAxisDuration: 0.9, easingOption: .easeInSine)
-        macroNutrientChart.legend.enabled = false
+//        macroNutrientChart.animate(yAxisDuration: 0.9, easingOption: .easeInSine)
+//        macroNutrientChart.legend.enabled = false
         print("Chart data: \(macroNutrientChart.data?.dataSets)")
         
         /// Keep as the last line
-        macroNutrientChart.notifyDataSetChanged()  
+        macroNutrientChart.notifyDataSetChanged()
+        return dataSet
+    }
+    
+    func formatChartValues(with dataSet: PieChartDataSet?) {
+        if let dataSet = dataSet {
+            let measurementFormatter = MeasurementFormatter()
+            let customValueFormatter = CustomValueFormatter(measurementFormatter: measurementFormatter)
+            dataSet.valueFormatter = customValueFormatter
+        }
+    }
+    
+    func styleMacroNutrientChart(with dataSet: PieChartDataSet?) {
+        
+        macroNutrientChart.animate(yAxisDuration: 0.9, easingOption: .easeInSine)
+        macroNutrientChart.legend.enabled = false
+        
+        if let dataSet = dataSet {
+            let chartLabelColor = UIColor(red:0.38, green:0.07, blue:0.33, alpha:1.0)
+            guard let customFont = UIFont(name:"HelveticaNeue-Bold", size: 18.0) else { return }
+            dataSet.colors = ChartColorTemplates.customTemplateBright()
+            dataSet.valueTextColor = chartLabelColor
+            dataSet.valueFont = customFont
+            dataSet.xValuePosition = .outsideSlice
+            dataSet.yValuePosition = .outsideSlice
+            dataSet.valueLineColor = chartLabelColor
+            dataSet.valueLinePart1Length = 0.45
+            dataSet.valueLinePart2Length = 0.1
+        }
     }
 }
