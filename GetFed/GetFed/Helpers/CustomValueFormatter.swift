6ca42b878e0b69e36a9aa6f84b1962fde98a38fe
Britney Smith <brit.smith7@gmail.com>
2018-12-03 15:34:55 -0500

    Refactor: property value closure

--- GetFed/GetFed/Helpers/CustomValueFormatter.swift
+++ GetFed/GetFed/Helpers/CustomValueFormatter.swift
@@ -11,17 +11,32 @@ import Charts
 
 public class CustomValueFormatter: NSObject, IValueFormatter {
     
-    private let measurementFormatter = MeasurementFormatter()
+//    private let measurementFormatter = MeasurementFormatter()
+//
+//    public func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+//
+//        let valueInGrams = Measurement(value: value, unit: UnitMass.grams)
+//        measurementFormatter.unitOptions = .providedUnit
+//
+//        let numberFormatter = NumberFormatter()
+//        numberFormatter.numberStyle = .none
+//        measurementFormatter.numberFormatter = numberFormatter
+//
+//        return measurementFormatter.string(for: valueInGrams)!
+//    }
     
-    public func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
-        
-        let valueInGrams = Measurement(value: value, unit: UnitMass.grams)
+    private var measurementFormatter: MeasurementFormatter = {
+        let measurementFormatter = MeasurementFormatter()
         measurementFormatter.unitOptions = .providedUnit
         
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .none
         measurementFormatter.numberFormatter = numberFormatter
-        
+        return measurementFormatter
+    }()
+    
+    public func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+        let valueInGrams = Measurement(value: value, unit: UnitMass.grams)
         return measurementFormatter.string(for: valueInGrams)!
     }
 }
