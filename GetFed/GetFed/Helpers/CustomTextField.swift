f05390d073aaa1b14738ff649b882eba73d9c885
Britney Smith <brit.smith7@gmail.com>
2018-12-07 14:43:19 -0500

    Added UITextField extension

---
+++ GetFed/GetFed/Helpers/CustomTextField.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTextField.swift
+//  GetFed
+//
+//  Created by Britney Smith on 12/7/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func underlined() {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        
+        border.borderColor = UIColor.lightGray.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: self.frame.size.height)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+}
