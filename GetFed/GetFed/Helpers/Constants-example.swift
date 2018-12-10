9eaa6a5ea32de6ef933ebd5d6b046d8026fb2c80
Britney Smith <brit.smith7@gmail.com>
2018-12-10 10:56:04 -0500

    Added Constants-example.swift file

---
+++ GetFed/GetFed/Helpers/Constants-example.swift
@@ -0,0 +1,13 @@
+//
+//  Constants-example.swift
+//  GetFed
+//
+//  Created by Britney Smith on 12/10/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+/// Replace with app key and app id from developer account (for the Food API) at https://www.edamam.com
+let EdamamAppID = "your-app-id"
+let EdamamAppKey = "your-app-key"
