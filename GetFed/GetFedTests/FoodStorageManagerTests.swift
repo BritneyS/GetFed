92b0da2c663da9c0771d5b6958ebfaf95f392700
Britney Smith <brit.smith7@gmail.com>
2019-01-18 15:07:58 -0500

    Push more changes for review

--- GetFed/GetFedTests/FoodStorageManagerTests.swift
+++ GetFed/GetFedTests/FoodStorageManagerTests.swift
@@ -23,33 +23,52 @@ public extension NSManagedObject {
 class FoodStorageManagerTests: XCTestCase {
     
     var systemUnderTest: FoodStorageManager!
-//    lazy var managedObjectModel: NSManagedObjectModel = {
-//        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: type(of: self))])!
-//        return managedObjectModel
+    var mockPersistentContainer: NSPersistentContainer? = nil
+    lazy var managedObjectModel1: NSManagedObjectModel = {
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: type(of: self))])!
+        return managedObjectModel
+    }()
+//    lazy var mockPersistentContainer: NSPersistentContainer = {
+//        let container = NSPersistentContainer(name: "GetFed", managedObjectModel: self.managedObjectModel)
+//        let description = NSPersistentStoreDescription()
+//        description.type = NSInMemoryStoreType /// in-memory store separate from production persistent store
+//        description.shouldAddStoreAsynchronously = false /// for testing
+//
+//        container.persistentStoreDescriptions = [description]
+//        container.loadPersistentStores { (description, error) in
+//
+//            /// Check if the data store is in memory
+//            precondition(description.type == NSInMemoryStoreType, "Failure in FoodStorageManagerTest : Data store not in memory")
+//
+//            if let error = error {
+//                fatalError("Creation of in-memory coodinator failed: \(error)")
+//            }
+//        }
+//        return container
 //    }()
-    lazy var mockPersistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "GetFed")//, managedObjectModel: self.managedObjectModel)
+    
+    func createMockPersistentContainer() {
+        mockPersistentContainer = NSPersistentContainer(name: "GetFed")//, managedObjectModel: self.managedObjectModel)
         let description = NSPersistentStoreDescription()
         description.type = NSInMemoryStoreType /// in-memory store separate from production persistent store
-        description.shouldAddStoreAsynchronously = false /// for testing
+        //description.shouldAddStoreAsynchronously = false /// for testing
         
-        container.persistentStoreDescriptions = [description]
-        container.loadPersistentStores { (description, error) in
-            
-            /// Check if the data store is in memory
-            precondition(description.type == NSInMemoryStoreType, "Failure in FoodStorageManagerTest : Data store not in memory")
-            
+        mockPersistentContainer!.persistentStoreDescriptions = [description]
+        mockPersistentContainer!.loadPersistentStores { (description, error) in
+        
+        /// Check if the data store is in memory
+        precondition(description.type == NSInMemoryStoreType, "Failure in FoodStorageManagerTest : Data store not in memory")
+        
             if let error = error {
                 fatalError("Creation of in-memory coodinator failed: \(error)")
             }
         }
-        return container
-    }()
+    }
     
     func createTestStubs() {
         func insertTestFood(label: String, brand: String, calories: Double, protein: Double, carbs: Double, fat: Double) -> Food? {
-            let foodItem = NSEntityDescription.insertNewObject(forEntityName: "Food", into: mockPersistentContainer.viewContext) as! Food
-            let nutrientItem = NSEntityDescription.insertNewObject(forEntityName: "Nutrients", into: mockPersistentContainer.viewContext) as! Nutrients
+            let foodItem = NSEntityDescription.insertNewObject(forEntityName: "Food", into: mockPersistentContainer!.viewContext) as! Food
+            let nutrientItem = NSEntityDescription.insertNewObject(forEntityName: "Nutrients", into: mockPersistentContainer!.viewContext) as! Nutrients
             let caloriesInt = Int(calories)
             let proteinInt = Int(protein)
             let carbsInt = Int(carbs)
@@ -75,7 +94,7 @@ class FoodStorageManagerTests: XCTestCase {
         insertTestFood(label: "food5", brand: "brand5", calories: 600, protein: 10, carbs: 50, fat: 25)
         
         do {
-            try mockPersistentContainer.viewContext.save()
+            try mockPersistentContainer!.viewContext.save()
         } catch {
             print("Save error: \(error)")
         }
@@ -83,27 +102,46 @@ class FoodStorageManagerTests: XCTestCase {
     
     func flushData() {
         let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "Food")
-        let records = try! mockPersistentContainer.viewContext.fetch(fetchRequest)
+        let records = try! mockPersistentContainer!.viewContext.fetch(fetchRequest)
         for case let record as NSManagedObject in records {
-            mockPersistentContainer.viewContext.delete(record)
+            mockPersistentContainer!.viewContext.delete(record)
         }
-        try! mockPersistentContainer.viewContext.save()
+        try! mockPersistentContainer!.viewContext.save()
+    }
+    
+    func destroyPersistentStore() {
+//        if let storeURL = mockPersistentContainer?.persistentStoreCoordinator.persistentStores.first?.url {
+//
+//            do {
+//                try mockPersistentContainer?.persistentStoreCoordinator.destroyPersistentStore(at: storeURL, ofType: NSInMemoryStoreType, options: nil)
+//            } catch {
+//                print("Failure in destroying test store: \(error)")
+//            }
+//
+//        } else {
+//            print("No store url")
+//        }
+        
+        mockPersistentContainer = nil
     }
 
     override func setUp() {
         super.setUp()
+        createMockPersistentContainer()
         createTestStubs()
-        systemUnderTest = FoodStorageManager(persistentContainer: mockPersistentContainer)
+        systemUnderTest = FoodStorageManager(persistentContainer: mockPersistentContainer!)
     }
 
     override func tearDown() {
         flushData()
+        destroyPersistentStore()
         super.tearDown()
     }
 
     func testCreateFoodEntry() {
 
         // given
+        //createMockPersistentContainer()
         let label = "Food6"
         let brand = "Brand6"
         let calories = 600.0
@@ -115,17 +153,20 @@ class FoodStorageManagerTests: XCTestCase {
         let insertedFood = systemUnderTest.insertFood(label: label, brand: brand, calories: calories, protein: protein, carbs: carbs, fat: fat)
 
         // then
+        //mockPersistentContainer = nil
         XCTAssertNotNil(insertedFood)
     }
     
     func testFetchAllFoodEntries() {
-        
+
         // given
-        
+        //createMockPersistentContainer()
+
         // when
         let results = systemUnderTest.fetchAll()
-        
+
         // then
+        //mockPersistentContainer = nil
         XCTAssertEqual(results.count, 5)
     }
 }
